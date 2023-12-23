224a8180131bc840956400529dad9eefca18a30b
Slow Coder <slowcoder@gmail.com>
2023-12-24 01:08:32 +0700

    good enough

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,27 @@ import { Tile } from "./Tile";
 import { useController } from "./controller";
 import { useInterval } from "./useInterval";
 import type { Dir, TileState, Coor } from "./types";
-import { flushSync } from "react-dom";
 
 const COLUMNS = 30;
 const ROWS = 20;
 
 const grid: number[][] = new Array(ROWS).fill(new Array(COLUMNS).fill(0));
 
-function randomGrid() {
+function randomTile(snake: Coor[]) {
   function rnd(n: number) {
     return Math.floor(Math.random() * n);
   }
 
-  return {
-    x: rnd(COLUMNS),
-    y: rnd(ROWS),
-  };
+  let tile = { x: 0, y: 0 };
+
+  do {
+    tile = {
+      x: rnd(COLUMNS),
+      y: rnd(ROWS),
+    };
+  } while (snake.some((i) => i.x === tile.x && i.y === tile.y));
+
+  return tile;
 }
 
 function moveSnake(snake: Coor[], dir: Dir) {
@@ -44,76 +49,49 @@ function moveSnake(snake: Coor[], dir: Dir) {
   return newSnake;
 }
 
-let tes = false;
-let val = {};
+function isGameOver(snake: Coor[]) {
+  const head = snake[0];
+
+  const collide = snake.slice(3).some((i) => i.x === head.x && i.y === head.y);
+
+  return (
+    collide ||
+    head.x < 0 ||
+    head.x > COLUMNS - 1 ||
+    head.y < 0 ||
+    head.y > ROWS - 1
+  );
+}
 
 function App() {
   const [snake, setSnake] = useState<Coor[]>([
     { x: 13, y: 15 },
     { x: 12, y: 15 },
   ]);
-  const [food, setFood] = useState<Coor>({ x: 15, y: 15 });
+  const [food, setFood] = useState<Coor>({ x: 17, y: 15 });
   const [dir, setDir] = useState<Dir>("right");
-
-  const gameOver =
-    snake[0].x <= 0 ||
-    snake[0].x >= COLUMNS - 1 ||
-    snake[0].y <= 0 ||
-    snake[0].y >= ROWS - 1;
+  const [gameOver, setGameOver] = useState(false);
 
   function moveOneStep() {
     if (gameOver) return;
 
-    let newHead = null;
-    let newSnake = null;
+    const newSnake = moveSnake(snake, dir);
+    const newHead = newSnake[0];
 
-    if (tes) {
-      console.log({ tes, val });
-      flushSync(() => {
-        setSnake([val, ...snake]);
-      });
-
-      newHead = val;
-      newSnake = snake;
-      tes = false;
-      val = {};
-    } else {
-      newSnake = moveSnake(snake, dir);
-      newHead = newSnake[0];
-
-      setSnake(newSnake);
+    if (isGameOver(newSnake)) {
+      setGameOver(true);
+      return;
     }
 
     if (newHead.x === food.x && newHead.y === food.y) {
-      const newHead: Coor = { x: 0, y: 0 };
-
-      if (dir === "left") {
-        newHead.x = newSnake[0].x - 1;
-        newHead.y = newSnake[0].y;
-      } else if (dir === "right") {
-        newHead.x = newSnake[0].x + 1;
-        newHead.y = newSnake[0].y;
-      } else if (dir === "up") {
-        newHead.x = newSnake[0].x;
-        newHead.y = newSnake[0].y - 1;
-      } else if (dir === "down") {
-        newHead.x = newSnake[0].x;
-        newHead.y = newSnake[0].y + 1;
-      }
-
-      // should set in next step
-      tes = true;
-      val = newHead;
-
-      console.log(val);
-
-      const a = randomGrid();
-      console.log(a);
-      setFood(a);
+      newSnake.push(snake[snake.length - 1]);
+      setFood(randomTile(newSnake));
     }
+
+    setSnake(newSnake);
   }
 
-  useInterval(moveOneStep, 300);
+  useInterval(moveOneStep, 200);
 
   useController(setNextDir);
 
@@ -158,4 +136,4 @@ function App() {
 
 export default App;
 
-// done for today, will refactor tomorrow
+// i think this is good enough, just need to make the ui prettier
